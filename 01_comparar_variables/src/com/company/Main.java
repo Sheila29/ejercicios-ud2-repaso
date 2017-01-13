3ecd4bcb0ed3b64b539eb1b53cd64c6e159e4760
sheilapsm <murgia85@gmail.com>
2017-01-13 17:46:08 +0100

    empezado

---
+++ 01_comparar_variables/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+
+	Escribe un programa que cree al azar tres variables enteras y
+	las compare entre sí, sacando en pantalla cual de ellas es la mayor,
+	la siguiente más grande y la menor.
+
+    Hay que tener en cuenta si son iguales, nunca se sabe.
+	 */
+
+        Random r = new Random();
+        int aleatorio = r.nextInt();
+        int aleatorio2 = r.nextInt();
+        int aleatorio3 = r.nextInt();
+
+
+        if (aleatorio > aleatorio2 )
+
+
+    }
+}
---
+++ 03_buscar_mayor_menor/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public void visualizar(int[] n){
+
+        System.out.println(numero[n]);
+    }
+    public static void main(String[] args) {
+	/*
+
+    visualizar(): muestra el array en pantalla.
+    buscarMayor(): muestra el número más alto que haya en el array.
+    buscarMenor(): muestra el número más pequeño que haya en el array.
+    existe(n): devuelve true si el valor buscado existe en el array.
+
+
+
+
+	 */
+        Random r = new Random();
+        int aleatorio = r.nextInt();
+
+	    int[] numero = new int[10];
+
+
+    }
+}
