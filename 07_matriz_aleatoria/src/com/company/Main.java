8e6a7f6d70745ced7ab99ec4575f1c043494b296
sheilapsm <murgia85@gmail.com>
2017-01-15 00:04:35 +0100

    empezado

---
+++ 07_matriz_aleatoria/src/com/company/Main.java
@@ -0,0 +1,89 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Pide al usuario filas y columnas
+        System.out.println("Filas: ");
+        int filas = Integer.parseInt(br.readLine());
+
+        System.out.println("Columnas: ");
+        int columnas = Integer.parseInt(br.readLine());
+
+        Random r = new Random();
+        int aleatorio = r.nextInt();
+
+        // Generar matriz de números enteros aleatorios
+        int[][] matriz = new int[filas][columnas];
+
+        for (int f = 0; f < filas ; f++) {
+            for (int c = 0; c < columnas; c++) {
+                matriz[f][c] = aleatorio;
+
+
+            }
+
+        }
+
+        // Muestra la matriz en pantalla
+            for (int f = 0; f < filas ; f++) {
+            for (int c = 0; c < columnas; c++) {
+                System.out.format("%2d", matriz[f][c]);
+
+            }
+            System.out.println();
+
+        }
+
+        /* Crea un array con el tamaño suficiente para almacenar todos los valores
+        de la matriz y copia en él los datos */
+
+        int[][] array = new int[matriz.length][matriz[0].length];
+
+        for (int f = 0; f < matriz.length ; f++) {
+            for (int c = 0; c < matriz[0].length; c++) {
+                array[f][c] = matriz[f][c];
+
+
+            }
+
+        }
+
+        // Muestra el array.
+        for (int f = 0; f < matriz.length ; f++) {
+            for (int c = 0; c < matriz[0].length; c++) {
+                System.out.format("%2d", array[f][c]);
+
+            }
+            System.out.println();
+
+        }
+
+        // Muestra el array eliminando los valores repetidos
+
+        for (int f = 0; f < ; f++) {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
