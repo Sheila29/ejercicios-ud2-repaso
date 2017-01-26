6e55318261bb58351de84f89c4c31ea09dd490b6
sheilapsm <murgia85@gmail.com>
2017-01-26 13:20:05 +0100

    terminado

--- 05_figuras/src/com/company/Main.java
+++ 05_figuras/src/com/company/Main.java
@@ -5,6 +5,13 @@ public class Main {
     public static void main(String[] args) {
 
 
+        /*
+        *
+        **
+        ***
+        ****
+        *****
+         */
         for (int i = 0; i < 5; i++) {
             System.out.print(" *");
 
@@ -13,6 +20,14 @@ public class Main {
 
         System.out.println();
 
+
+        /*
+        *****
+        *****
+        *****
+        *****
+        *****
+         */
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print("*");
@@ -21,6 +36,14 @@ public class Main {
             System.out.println();
         }
 
+        /*
+        *
+        **
+        ***
+        ****
+        *****
+         */
+
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
@@ -28,12 +51,136 @@ public class Main {
         System.out.println();
     }
 
-        for (int i = 5; i > 0 ; i--) {
-            for (int j = i; j > 0; j--) {
-                System.out.println("*");
+        System.out.println();
+
+    /*
+    *****
+    ****
+    ***
+    **
+    *
+
+     */
+
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+            for (int j = 0; j < 4; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+            for (int j = 0; j < 3; j++) {
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+            for (int j = 0; j < 2; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+            for (int j = 0; j < 1; j++) {
+                System.out.print("*");
 
             }
             System.out.println();
         }
+
+        System.out.println();
+
+        /*
+               *
+                *
+                 *
+                  *
+                   *
+         */
+
+        for (int i = 0; i < 1; i++) {
+            for (int j = 0; j < 1; j++) {
+                System.out.print("*");
+                
+            }
+
+            System.out.println();
+
+            for (int j = 0; j < 1; j++) {
+                System.out.print(" ");
+                
+            }
+            for (int j = 0; j < 1; j++) {
+                System.out.print("*");
+
+            }
+
+            System.out.println();
+
+            for (int j = 0; j < 1; j++) {
+                System.out.print("  ");
+
+            }
+            for (int j = 0; j < 1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+            for (int j = 0; j < 1; j++) {
+                System.out.print("   ");
+
+            }
+            for (int j = 0; j < 1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+
+            for (int j = 0; j < 1; j++) {
+                System.out.print("    ");
+
+            }
+            for (int j = 0; j < 1; j++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        /*
+        *****
+        *   *
+        *   *
+        *   *
+        *****
+
+         */
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("*");
+
+        }
+        System.out.println();
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print("*   *");
+            System.out.println();
+
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("*");
+
+        }
+
     }
 }
